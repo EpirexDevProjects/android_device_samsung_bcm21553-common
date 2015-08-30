a815e61fb2da023a262288393c9339959b783013
Rafael PacMe <rafy.pachecomendez@gmail.com>
2015-08-29 22:43:16 -0300

    arreglando recovery configs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,21 +40,20 @@ ARM_EABI_TOOLCHAIN						:= $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/arm
 TARGET_KERNEL_SOURCE					:= kernel/samsung/bcm21553-common
 
 # Recovery
-ifneq ($(RECOVERY_VARIANT),twrp)
-	TARGET_RECOVERY_FSTAB			:= device/samsung/bcm21553-common/recovery/fstab.cm.bcm21553
+ifneq (,$(filter cori,$(CM_BUILD)))
+TARGET_RECOVERY_FSTAB				:= device/samsung/bcm21553-common/recovery/mmc/recovery.fstab
+TARGET_USERIMAGES_USE_EXT4			:= true
+BOARD_FLASH_BLOCK_SIZE				:= 4096
 else
-	TARGET_RECOVERY_FSTAB			:= device/samsung/bcm21553-common/recovery/fstab.twrp.bcm21553
-endif
-BOARD_BML_BOOT					:= "/dev/block/bml7"
-BOARD_BML_RECOVERY				:= "/dev/block/bml7"
-BOARD_CUSTOM_RECOVERY_KEYMAPPING		:= ../../device/samsung/bcm21553-common/recovery/bcm21553_recovery_keys.c
+TARGET_RECOVERY_FSTAB				:= device/samsung/bcm21553-common/recovery/mtd/recovery.fstab
 BOARD_FLASH_BLOCK_SIZE				:= 131072
+endif
+BOARD_CUSTOM_RECOVERY_KEYMAPPING		:= ../../device/samsung/bcm21553-common/recovery/recovery_ui.c
 BOARD_RECOVERY_HANDLES_MOUNT			:= true
 BOARD_HAS_DOWNLOAD_MODE				:= true
-TARGET_RECOVERY_PIXEL_FORMAT			:= BGRA_8888
+TARGET_RECOVERY_PIXEL_FORMAT			:= "BGRA_8888"
 TARGET_NO_SEPARATE_RECOVERY			:= true
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH		:= \"/sys/class/backlight/aat1401-backlight/brightness\"
-DEVICE_RESOLUTION := 320x480
 
 # TWRP
 ifeq ($(RECOVERY_VARIANT),twrp)
